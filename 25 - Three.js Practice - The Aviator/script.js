ae3aed1cc8f1e4191090fc830ef8d3782b628cdb
Fr3ud <oo.pavelko@gmail.com>
2019-02-12 20:41:25 +0200

    Part2

--- 25 - Three.js Practice - The Aviator/script.js
+++ 25 - Three.js Practice - The Aviator/script.js
@@ -1,5 +1,41 @@
+const colors = {
+  wheat: 0xf7d9aa,
+}
+
+const createScene = function() {
+  WIDTH = window.innerWidth;
+  HEIGHT = window.innerHeight;
+
+  /**SCENE */
+  scene = new THREE.Scene();
+  scene.fog = new THREE.Fog(colors.wheat, 100, 950);
+
+  /**CAMERA */
+  fieldOfView = 60;
+  aspectRatio = WIDTH / HEIGHT;
+  nearPlane = 1;
+  farPlane = 10000;
+
+  camera = new THREE.PerspectiveCamera(
+    fieldOfView,
+    aspectRatio,
+    nearPlane,
+    farPlane,
+  );
+  camera.position.set(0, 100, 200);
+
+  /**RENDERER */
+  renderer = new THREE.WebGLRenderer({
+    alpha: true,
+    antialias: true,
+  });
+  renderer.setSize(WIDTH, HEIGHT);
+  container = document.getElementById('world');
+  container.appendChild(renderer.domElement);
+}
+
 const init = function() {
-  
+  createScene();
 }
 
 window.addEventListener('load', init);
